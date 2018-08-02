ea960df2a74d32923f454339178c83f7fceeb56b
Bobby <bobby077joe@gmail.com>
2018-08-02 13:08:15 -0700

    Added signin and signout.

--- src/index.js
+++ src/index.js
@@ -9,15 +9,27 @@ import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 import reducers from './reducers';
+import Feature from './components/Feature';
+import Signout from './components/auth/Signout';
+import Signin from './components/auth/Signin';
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+// Reducer, Initial state, middleware.
+
+const store = createStore(
+  reducers,
+  { auth: { authenticated: localStorage.getItem('token') } },
+  applyMiddleware(reduxThunk)
+);
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <App>
-        <Route path="/" component={Welcome} />
+        <Route exact path="/" component={Welcome} />
         <Route path="/signup" component={Signup} />
+        <Route path="/feature" component={Feature} />
+        <Route path="/signout" component={Signout} />
+        <Route path="/signin" component={Signin} />
       </App>
     </BrowserRouter>
   </Provider>,
